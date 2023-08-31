bdab2b145d604aa952347a804eedbc76029621c7
Andy Geach <andy@bugorfeature.net>
2023-08-31 12:23:51 +0100

    also clear backtest statefiles from offline backup

--- sysproduction/clean_truncate_backtest_states.py
+++ sysproduction/clean_truncate_backtest_states.py
@@ -1,5 +1,6 @@
 from syscore.fileutils import delete_old_files_with_extension_in_pathname
 from sysproduction.data.backtest import get_directory_store_backtests
+from sysdata.config.production_config import get_production_config
 from sysdata.data_blob import dataBlob
 
 
@@ -18,7 +19,7 @@ class cleanTruncateBacktestStates:
     def clean_backtest_states(self):
         directory_to_use = get_directory_store_backtests()
         self.data.log.debug(
-            "Deleting old .pck and .yaml backtest state files in directory %s"
+            "Deleting old .pck and .yaml backtest state files in directory '%s'"
             % directory_to_use
         )
         delete_old_files_with_extension_in_pathname(
@@ -26,6 +27,20 @@ class cleanTruncateBacktestStates:
         )
         delete_old_files_with_extension_in_pathname(
             directory_to_use, days_old=3, extension=".yaml"
+        )
+
+        # also clear offline backup
+        production_config = get_production_config()
+        offline = production_config.get_element("offsystem_backup_directory")
+        self.data.log.debug(
+            f"Deleting old .pck and .yaml backtest state files in directory '{offline}'"
+
+        )
+        delete_old_files_with_extension_in_pathname(
+            offline, days_old=3, extension=".pck"
+        )
+        delete_old_files_with_extension_in_pathname(
+            offline, days_old=3, extension=".yaml"
         )
 
 
