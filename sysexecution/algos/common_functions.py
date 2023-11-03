6e1484833b9804a738d485222777093c9a30465c
Andy Geach <andy@bugorfeature.net>
2023-11-03 13:52:08 +0000

    reduce excessive logging during 'manage trade' loop

--- sysexecution/algos/common_functions.py
+++ sysexecution/algos/common_functions.py
@@ -61,13 +61,12 @@ def cancel_order(
 
 
 def set_limit_price(
-    data: dataBlob,
+    data_broker: dataBroker,
     broker_order_with_controls: orderWithControls,
     new_limit_price: float,
 ):
 
     log_attrs = {**broker_order_with_controls.order.log_attributes(), "method": "temp"}
-    data_broker = dataBroker(data)
 
     try:
         broker_order_with_controls = (
@@ -75,12 +74,12 @@ def set_limit_price(
                 broker_order_with_controls, new_limit_price
             )
         )
-        data.log.debug(
+        data_broker.data.log.debug(
             "Tried to change limit price to %f" % new_limit_price,
             **log_attrs,
         )
     except orderCannotBeModified as error:
-        data.log.debug(
+        data_broker.data.log.debug(
             "Can't modify limit price for order, error %s" % str(error),
             **log_attrs,
         )
